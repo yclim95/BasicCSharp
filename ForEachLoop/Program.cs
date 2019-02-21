5b400a3eeaa23145eb71649c527e6a889528bdc3
YC Lim <lyc950421@gmail.com>
2019-02-21 21:54:38 +0800

    Initial Commit

---
+++ ForEachLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []numbers = {0,1,2,3,4,5,6,7,8,9};
+
+            foreach(int number in numbers){
+                Write(number);
+            }
+        }
+    }
+}
